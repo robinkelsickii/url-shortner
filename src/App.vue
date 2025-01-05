aa0c229a096feb46e41532cd0f44196c75b02d42
robinkii <robinkelsickii@gmail.com>
2025-01-04 20:10:37 -0500

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to Vue!</h1>
+    <p>This is your starting template.</p>
+  </div>
+</template>
